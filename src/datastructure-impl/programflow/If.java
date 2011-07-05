111e56c02af6d0893c499f1ffd73c644d7240dc9
Hubert <vevusio@gmail.com>
2011-07-05 19:02:02 +0200

    program flow stuff

--- src/datastructure-impl/programflow/If.java
+++ src/datastructure-impl/programflow/If.java
@@ -1,11 +1,22 @@
 package programflow;
 
+import programflow.Instruction;
+
 import javax.sound.midi.Sequence;
 import java.lang.String;
 
 public class If extends Construct {
+	private String condition;
+	private Instruction instruction;
+
+	public If(String condition) {
+		this.condition = condition;
+	}
 
 	public String translate() {
-		return null;
+		String result = "if(" + condition + ") {\r\n";
+		result += instruction.translate() + "\r\n";
+		result += "}\r\n";
+		return result;
 	}
 }
