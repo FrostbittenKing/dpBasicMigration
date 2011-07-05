1472e75fcb4a08553ec2820feab70525bc6f47c1
Grillkohle <Grillkohle@.(none)>
2011-07-05 19:11:11 +0200

    added a bunch of function implementation classes

---
+++ src/datastructure-impl/programflow/Log.java
@@ -0,0 +1,22 @@
+package programflow;
+
+public class Log extends Instruction{
+	private Double value;
+	private String varName;
+	
+	public Log(Double value){
+		this.value = value;
+	}
+	
+	public Log(String varName){
+		this.varName = varName;
+	}
+	
+	@Override
+	public String translate(){
+		if(value != null){
+			return "Math.log(" + value + ");\r\n";
+		else
+			return "Math.log(" + varName + ");\r\n";
+	}
+}
---
+++ src/datastructure-impl/programflow/Sin.java
@@ -0,0 +1,22 @@
+package programflow;
+
+public class Sin extends Instruction{
+	private Double value;
+	private String varName;
+	
+	public Sin(Double value){
+		this.value = value;
+	}
+	
+	public Sin(String varName){
+		this.varName = varName;
+	}
+	
+	@Override
+	public String translate(){
+		if(value != null){
+			return "Math.sin(" + value + ");\r\n";
+		else
+			return "Math.sin(" + varName + ");\r\n";
+	}
+}
