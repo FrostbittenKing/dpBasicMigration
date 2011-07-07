fe04b273bd1ee8353ca243193b77d7109da38bfd
Grillkohle <Grillkohle@.(none)>
2011-07-07 18:00:22 +0200

    added more basic functions

---
+++ src/datastructure-impl/programflow/Val.java
@@ -0,0 +1,22 @@
+package programflow;
+
+public class Val extends Instruction{
+	private String varName;
+	
+	public Val(String varName){
+		this.varName = varName;
+	}
+	
+	@Override
+	public String translate(){
+		try {
+			return "Double.parseDouble(" + varName) + ");\r\n";
+		}
+		catch (NullPointerException e){
+			return "0";
+		}
+		catch (NumberFormatException e){
+			return "0";
+		}
+	}
+}
