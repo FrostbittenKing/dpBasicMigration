4f9b00b0b55ac0b56628935c70fd2df38fae4455
Grillkohle <Grillkohle@.(none)>
2011-07-30 14:41:55 +0200

    print

--- src/datastructure-impl/basicparser/programflow/Print.java
+++ src/datastructure-impl/basicparser/programflow/Print.java
@@ -1,6 +1,9 @@
 package basicparser.programflow;
 
 import basicparser.ASTExpression;
+import basicparser.ASTSpace;
+import basicparser.ASTTab;
+import basicparser.emu.BasicEmu;
 
 public class Print extends Instruction{
 	private ASTExpression [] printExpressions;
@@ -11,7 +14,20 @@ public class Print extends Instruction{
 	
 	@Override
 	public String translate(){
-		String printText = null;
+		String printText = "BasicEmu.instance().startPrint();\r\n";
+		
+		for (int i = 0; i < printExpressions.length; i++){
+			if (printExpressions[i].jjtGetChild(0).jjtGetChild(0) instanceof ASTTab){
+				printText += "BasicEmu.instance().tab(" + ((ASTTab)printExpressions[i].jjtGetChild(0).jjtGetChild(0)).getParameterVomTab() + ");\r\n";
+			}
+			else if (printExpressions[i].jjtGetChild(0).jjtGetChild(0) instanceof ASTSpace){
+				printText += "BasicEmu.instance().spc(" + ((ASTSpace)printExpressions[i].jjtGetChild(0).jjtGetChild(0)).getParameterVomSpace() + ");\r\n";
+			}
+			else if //do normal print text
+		}
+		
+		
+		/*
 		if(printExpressions.length > 0) {
 			printText= ExpressionTranslator.instance().translate(printExpressions[0]);
 		}
@@ -19,8 +35,9 @@ public class Print extends Instruction{
 		for (int i = 1; i < printExpressions.length; i++) {
 			printText += " + \" \" + " + ExpressionTranslator.instance().translate(printExpressions[i]);
 		}
+		*/
 	
-		return "System.out.println(" + printText + ");";
+		return printText;
 	}
 }
 
